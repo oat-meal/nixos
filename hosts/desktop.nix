65214ebe78c39cc5dcda5e87d55fbe859a9cda81
Christopher Seiber <christopher.seiber@gmail.com>
2025-11-11 13:18:40 -0700

    Update desktop.nix
    Enhancing btrfs subvol configuration

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -1,71 +1,113 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Target system compatibility version (lock system modules to this release)
+  ########################################
+  ## System Core
+  ########################################
+
   system.stateVersion = "25.05";
-
-  # Enable flake support (required for flake-based system configuration)
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Allow installation of non-free software (e.g., Steam, proprietary drivers)
   nixpkgs.config.allowUnfree = true;
 
-  # Localization settings
-  i18n.defaultLocale = "en_US.UTF-8";
+  ########################################
+  ## Locale & Time
+  ########################################
 
-  # Set system timezone
+  i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "America/Chicago";
 
-  # Bootloader configuration for UEFI systems using systemd-boot
+  ########################################
+  ## Bootloader (UEFI + systemd-boot)
+  ########################################
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Filesystem configuration using UUIDs for stability
-  fileSystems."/" = {
-    device = "UUID=8ea2530c-aaee-4a0f-a1f4-0eed4c3ab74a";
-    fsType = "btrfs";
+  ########################################
+  ## Filesystem Configuration
+  ########################################
+  # EFI partition: /dev/nvme2n1p1 (FAT32, 2GiB, flags boot,esp)
+  # Root partition: /dev/nvme2n1p2 (Btrfs with subvols @, @home, @nix, @log, @swap)
+
+  fileSystems = {
+    "/" = {
+      device = "UUID=8ea2530c-aaee-4a0f-a1f4-0eed4c3ab74a";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+
+    "/boot" = {
+      device = "UUID=025C-FCOD";
+      fsType = "vfat";
+    };
+
+    "/home" = {
+      device = "UUID=8ea2530c-aaee-4a0f-a1f4-0eed4c3ab74a";
+      fsType = "btrfs";
+      options = [ "subvol=@home" ];
+    };
+
+    "/nix" = {
+      device = "UUID=8ea2530c-aaee-4a0f-a1f4-0eed4c3ab74a";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" ];
+    };
+
+    "/var/log" = {
+      device = "UUID=8ea2530c-aaee-4a0f-a1f4-0eed4c3ab74a";
+      fsType = "btrfs";
+      options = [ "subvol=@log" ];
+    };
   };
 
-  fileSystems."/boot" = {
-    device = "UUID=025C-FCOD";
-    fsType = "vfat";
-  };
+  # Swap file will live on /swap/swapfile (inside subvol=@swap)
+  swapDevices = [
+    { file = "/swap/swapfile"; }
+  ];
 
-  # Set hostname and enable NetworkManager for all network interfaces
+  ########################################
+  ## Hostname & Networking
+  ########################################
+
   networking.hostName = "desktop-nixos";
   networking.networkmanager.enable = true;
 
-  # Define a regular user account with sudo and device access
+  ########################################
+  ## User Configuration
+  ########################################
+
   users.users.chris = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "audio" "video" ];
     shell = pkgs.zsh;
-
-    # Suppress shell path warning for Zsh (safe for managed environments)
     ignoreShellProgramCheck = true;
   };
 
-  # Enable Zsh globally for all users
   programs.zsh.enable = true;
 
-  # Enable Bluetooth support (hardware and GUI)
+  ########################################
+  ## Hardware & Services
+  ########################################
+
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  # Enable access to non-free firmware blobs (required for some Wi-Fi/GPUs)
   hardware.enableRedistributableFirmware = true;
 
-  # Common system utilities installed globally
+  ########################################
+  ## Global Packages
+  ########################################
+
   environment.systemPackages = with pkgs; [
-    git            # Git version control
-    wget           # Command-line download tool
-    curl           # HTTP client
-    btrfs-progs    # Tools for Btrfs filesystem
-    zsh            # Z Shell
-    neovim         # Terminal-based code editor
-    alacritty      # GPU-accelerated terminal emulator
-    pciutils       # lspci and other PCI tools
-    usbutils       # lsusb and other USB tools
-    lsb-release    # For checking distribution information
+    git
+    wget
+    curl
+    btrfs-progs
+    zsh
+    neovim
+    alacritty
+    pciutils
+    usbutils
+    lsb-release
   ];
 }
