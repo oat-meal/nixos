88803335f76b62f5ad30eac6ede2a9b649fa0848
Christopher Seiber <christopher.seiber@gmail.com>
2025-11-13 09:15:14 -0600

    Update niri.nix

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,34 +1,37 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Enable Niri Wayland compositor support
   programs.niri.enable = true;
 
-  # Install required environment tools for Wayland sessions
   environment.systemPackages = with pkgs; [
     niri
     xwayland
+    xwayland-satellite
     wl-clipboard
     wayland-utils
     xdg-utils
-    mako  # Wayland-compatible notification daemon
+    mako
   ];
 
-  # Enable core desktop services and interop with Wayland
   services.dbus.enable = true;
   services.libinput.enable = true;
 
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+  };
 
-  # Keyring support for GNOME-style secrets and GPG
   services.gnome.gnome-keyring.enable = true;
 
-  # Optional login manager using greetd with TUI frontend
   services.greetd = {
     enable = true;
-    settings.default_session.command = ''
-      ${pkgs.greetd.tuigreet}/bin/tuigreet --cmd niri
-    '';
+
+    settings = {
+      default_session = {
+        user = "chris";
+        command = "${pkgs.niri}/bin/niri";
+      };
+    };
   };
 }
+
