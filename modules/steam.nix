92eb429ca019b387dd969f9fd6bae30c1da73305
Christopher Seiber <christopher.seiber@gmail.com>
2025-11-13 09:16:23 -0600

    Update steam.nix

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,24 +1,52 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Enable Steam with necessary firewall ports for streaming and servers
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
-
-  # Enable 32-bit OpenGL support required for Proton and older games
   hardware.graphics = {
     enable = true;
-    extraPackages = with pkgs; [ vaapiVdpau libvdpau-va-gl ];
+    enable32Bit = true;
   };
 
-  # Install additional gaming tools and runtime libraries
-  environment.systemPackages = with pkgs; [
-    steam
-    wineWowPackages.staging  # Latest 64/32-bit Wine packages
-    lutris                   # Game launcher and Wine frontend
-    mangohud                 # Real-time game performance overlay
+  programs.steam = {
+    enable = true;
+
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+
+
+  extraPackages = with pkgs; [
+    # Audio
+    libpulseaudio
+    alsa-lib
+    # compatibility
+    gamescope
+    # Video / Input
+    libudev0-shim
+    libdrm
+    libxkbcommon
+    vulkan-loader
+    vulkan-tools
+
+    # X11
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXext
+    xorg.libxcb
+
+    # Wayland
+    wayland
+    wayland-protocols
+
+    # Toolkits
+    gtk3
+    SDL2
   ];
+
+};
+
+  # Optional: support custom proton builds
+  environment.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+    "$HOME/.steam/root/compatibilitytools.d";
 }
+
